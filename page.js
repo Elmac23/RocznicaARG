a681db59d293ef787cb5930e6a7700fb8419860b
Elmac23 <87617724+Elmac23@users.noreply.github.com>
2021-07-18 21:06:40 +0200

    Upload

---
+++ page.js
@@ -0,0 +1,16 @@
+    var submitBtn = document.getElementById("submitBtn");
+    submitBtn.addEventListener("click", SubmitKeys);
+
+function SubmitKeys() {
+    let key1 = document.getElementById("key1").value;
+    let key2 = document.getElementById("key2").value;
+    let key3 = document.getElementById("key3").value;
+    let key4 = document.getElementById("key4").value;
+    let key5 = document.getElementById("key5").value;
+    let keySequence = key1 + key2 + key3 + key4 + key5;
+
+    if (keySequence === "skarbbporsch3albo2k0cham37mix0") {
+        location.href = 'https://www.facebook.com/';
+    }
+
+}
